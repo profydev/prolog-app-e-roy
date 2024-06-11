38ce7ece7dabdd0d9cb0b19bce49e32bd828905c
e-roy <eric@fullstackeric.com>
2024-06-11 12:10:08 -0400

    remove extra loading import

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -2,7 +2,6 @@ import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import { Loading } from "@features/ui";
 import styles from "./project-list.module.scss";
-import { Loading } from "@features/ui";
 
 export function ProjectList() {
   const { data, isLoading, isError, error } = useGetProjects();
